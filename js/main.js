785f47765cba1db98e2ef3924551f4b3c1a6c902
Dmitri Zemliakov <112906798+ZemliakovD@users.noreply.github.com>
2024-07-03 20:36:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(function(){
+
+    $('.program-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button type="button" class="slick-prev prorgam-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next prorgam-next">Next</button>',
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+
+});
